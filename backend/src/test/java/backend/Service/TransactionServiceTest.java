ba7309ea92d3b89167c6b660b7ec727f178ec186
Bartlomiej Ciach <203860@edu.p.lodz.pl>
2018-05-06 21:57:11 +0200

    Add tests for getAllTransactions method from service

---
+++ backend/src/test/java/backend/Service/TransactionServiceTest.java
@@ -0,0 +1,95 @@
+package backend.Service;
+
+import backend.datastore.dao.TransactionRepository;
+import backend.datastore.entities.Category;
+import backend.datastore.entities.Transaction;
+import backend.dto.TransactionInfo;
+import backend.service.TransactionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TransactionServiceTest {
+    @TestConfiguration
+    static class TransactionServiceTesttContextConfiguration {
+
+        @Bean
+        public TransactionService transactionService() {
+            return new TransactionService();
+        }
+    }
+
+    @Autowired
+    private TransactionService service;
+
+    @MockBean
+    private TransactionRepository transactionRepository;
+
+
+    @Test
+    public void testNumberOfTransactions() {
+        int count = service.getAllTransactions().size();
+        assertEquals(3, count);
+    }
+
+    @Test
+    public void testDescriptionOfTransactions() {
+        List<String> expectedNames = Arrays.asList("des1", "des2", "des3");
+
+        List<String> names =  service.getAllTransactions()
+                .stream()
+                .map(TransactionInfo::getDescription)
+                .collect(Collectors.toList());
+
+        assertEquals(expectedNames,names);
+    }
+
+
+    @Before
+    public void init(){
+        Category c1 = Category.builder()
+                .id(1L)
+                .name("cat1")
+                .build();
+        Category c2 = Category.builder()
+                .id(2L)
+                .name("cat2")
+                .build();
+        Transaction t1 = Transaction.builder()
+                .id(1L)
+                .category(c1)
+                .description("des1")
+                .value(100.02F)
+                .build();
+        Transaction t2 = Transaction.builder()
+                .id(2L)
+                .category(c1)
+                .description("des2")
+                .value(-10F)
+                .build();
+        Transaction t3 = Transaction.builder()
+                .id(3L)
+                .category(c2)
+                .description("des3")
+                .value(-15F)
+                .build();
+        Mockito.when(transactionRepository.findAll()).thenReturn(Arrays.asList(t1,t2,t3));
+    }
+
+
+}
